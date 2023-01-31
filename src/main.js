ad0ce622081744e8439a9506dc338a05f8fe9e9d
WangMinan <wangminan0811@hotmail.com>
2023-01-31 14:00:47 +0800

    关闭mock

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import store from './store/index.js'
-import './api/mock.js'
+// import './api/mock.js'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 // 引入黑夜模式
 import 'element-plus/theme-chalk/dark/css-vars.css'
